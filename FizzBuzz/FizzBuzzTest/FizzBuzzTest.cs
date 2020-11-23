045fa830582c1699c6fbdad7ad513637ada7cb66
John Andersson <JohnA@centiro.se>
2020-11-23 12:18:30 +0100

    initialize project

---
+++ FizzBuzz/FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void FizzBuzzTestMethod()
+        {
+        }
+    }
+}
